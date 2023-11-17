b71c2c3ccefd572f75b410eac310b8ff8bdefc48
lebrozaurjagger <lebrozaur@icloud.com>
2023-11-17 15:29:18 +0300

    Initial Commit

---
+++ FireMaps/FireMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireMapsApp.swift
+//  FireMaps
+//
+//  Created by Phillip Jagger on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FireMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
